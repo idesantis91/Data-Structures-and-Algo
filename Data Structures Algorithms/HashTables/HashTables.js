9d9f8fffec7199894759160c1fdbccf37d685139
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-02-05 19:55:48 -0500

    Add files via upload

---
+++ Data Structures Algorithms/HashTables/HashTables.js
@@ -0,0 +1,14 @@
+//Hash Table
+let user ={
+    age: 54,
+    name: 'Isaiah',
+    magic: true,
+    scream: function(){
+        console.log("ahhhhhhh");
+    }
+}
+
+user.age //0(1)
+user.spell = "abra kadabra"
+console.log(user);
+user.scream(); //0(1)
